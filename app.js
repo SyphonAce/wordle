659e49bb1d6a6d3d71a9a92b2999b8b561889591
Kayne <Kayne@webdev11.local>
2024-03-04 11:01:27 -0500

    Boxes

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const secret = "aback";
+
 const letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
 const alpha = new Set(letters.split(''));
 
+let tries = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [], // 6 tries
+];
 
+let attempt = 0;
 
 
 
@@ -13,16 +23,23 @@ function logKey(evt) {
 
   if(evt.key.toUpperCase() == "ENTER") {
     console.log('submit');
+    if (attempt < 6 && tries[attempt].length == 5) {
+      attempt++;
+    }
   }
 
   if (evt.key.toUpperCase() == "BACKSPACE") {
-    console.lot('deleting');
+    console.log('deleting');
+    tries[attempt].pop();
   }
      if (alpha.has(evt.key.toUpperCase())) {
-       console.log(evt.key.toUpperCase());
+      if (tries[attempt].length <5){
+        tries[attempt].push( evt.key.toUpperCase() )
+      }
       } else {
         console.log('not a letter');
       } 
+      render();
 }
 
 const keyboard = [
@@ -38,19 +55,29 @@ keyboard[2].push(`BACK`);
 
 function render() {
   const main = document.querySelector('#root');
-  let template = `<div class="keyboard">`;
+  let board = `<div class="board">`;
 
-  for (let i = 0; i<keyboard.length; i++) {
-    template += `<div class="row">`
+  for (let i = 0; i<tries.length; i++) {
+   for (let j=0; j<5; j++) {
+        board += `<div>${ tries[i][j] ? tries[i][j] : ""}</div>`;
+   }
+   }
+   board += `</div>`;
+    let keyTemplate = `<div class="keyboard">`;
+
+    for (let i = 0; i<keyboard.length; i++) {
+    keyTemplate += `<div class="row">`
     for (let j=0; j<keyboard[i].length;j++){
-      template += `<div class="key">${ keyboard [i][j] }</div>`
+      keyTemplate += `<div class="key">${ keyboard [i][j] }</div>`
     }
-    template += `</div>`
+    keyTemplate += `</div>`
   }
 
-  template += `</div>`
+  keyTemplate += `</div>`
 
-  main.innerHTML = template;
+  let template = board + keyTemplate;
+
+  main.innerHTML =  template;
 
   console.log(main);
 
